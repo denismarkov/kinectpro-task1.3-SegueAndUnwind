0cb39fe8563e0af41be45576e14451770856ad77
Denis Markov <keynsianets@gmail.com>
2018-07-20 10:35:00 +0300

    Add .gitignore file

--- SegueAndUnwind/FoodVC.swift
+++ SegueAndUnwind/FoodVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FoodVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ SegueAndUnwind/KeyboardHideExtension.swift
@@ -0,0 +1,9 @@
+//
+//  KeyboardHideExtension.swift
+//  SegueAndUnwind
+//
+//  Created by Denis Markov on 7/20/18.
+//  Copyright © 2018 Denis Markov. All rights reserved.
+//
+
+import Foundation
